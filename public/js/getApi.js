538699449d50945da3f8763c7867fe67cd7df100
thanhhien234 <doanthanhhien01@gmail.com>
2024-02-08 02:28:04 +0900

    feat: add read-more-button

--- public/js/getApi.js
+++ public/js/getApi.js
@@ -1,4 +1,22 @@
 let booksData = [];
+let currentIndex = 0;
+
+function showBooks() {
+    for (let i = currentIndex; i < currentIndex + 3 && i < booksData.length; i++) {
+        $(".recommended-wrapper").addClass("show-animation");
+        const book = booksData[i];
+        $(".recommended-wrapper").append(`
+            <li class="item-wrapper">
+                <img src="${book.imageNum}">
+                <p class="title">${book.title}</p>
+            </li>
+        `);
+    }
+    currentIndex += 3;
+    if (currentIndex >= booksData.length) {
+        $("#read-more-button").text("줄이기");
+    }
+}
 
 async function getBook() {
     await $.ajax({
@@ -6,19 +24,13 @@ async function getBook() {
         type: "GET",
         success: function(data){
             booksData = data;
+            showBooks();
             $.each(data, function(index, book){
-                if (index < 3) {    // recommend first 3 books in home page
-                    $(".recommended-wrapper").append(`
-                        <li class="item-wrapper">
-                            <img src="${book.imageNum}">
-                            <p class="title">${book.title}</p>
-                        </li>
-                    `);
-                }
                 $(".main-container").append(`
                     <li class="item-wrapper" data-bookid="${book.id}">
                         <img src="${book.imageNum}">
                         <p class="title">${book.title}</p>
+                        <p class="author">${book.author}</p>
                         <div class="item-price">
                             <p class="price">${book.price}원</p>
                             <button class="cart-btn" type="button">장바구니</button>
@@ -33,4 +45,16 @@ async function getBook() {
     });
 }
 
+$("#read-more-button").on("click", function() {
+    if ($(this).text() === "줄이기") {
+        $(".recommended-wrapper").empty();
+        currentIndex = 0;
+        showBooks();
+        $(this).text("더보기");
+    } else {
+        showBooks();
+    }
+});
+
 getBook();
+
